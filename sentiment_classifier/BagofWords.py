8a1fa420eddc1eb7c00ac28fa2f5ad8c3f380c1a
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-03-17 13:42:11 -0400

    vocab complete and added reset for multiple uses

--- sentiment_classifier/BagofWords.py
+++ sentiment_classifier/BagofWords.py
@@ -3,34 +3,45 @@ import os
 
 class BagofWords(object):
     def __init__(self, fp, stopfp, ngrams):
-        """Initial constructor."""
         self.fp = fp
         self.stopfp = stopfp
         self.ngrams = ngrams
+        self.totaldata = []
+        self.vocabulary = {}
+        self.wordcount = 0
 
     def read(self):
+        print("*  reading stop list data")
         #  Read the stop list.
         stoplist = open(self.stopfp, 'r+', encoding='utf-8')
         #  Input buffer.
         stoplist = stoplist.read()
         stoplist = stoplist.split(',')
 
+        print("*  reading data in file path")
         for filename in os.listdir(self.fp):
-            # open the file in directory given
+            #  Open the file in directory given.
             filename = self.fp + filename
             file = open(filename, 'r+', encoding='utf-8')
-            # input buffer
+            #  Input buffer
             file = file.read()
-            print("Original file:")
-            print(file)
+            #  Tokenize words.
             file = self.tokenize(file)
+            #  Split words
             words = file.split(' ')
+            #  If there are n-grams, then apply that. Otherwise implement stoplist.
             if self.ngrams > 1:
                 words = self.split_ngrams(words)
-            words = self.stopwords(words, stoplist)
-            #  Split up the words.
-            print("Modified file:")
-            print(words)
+            else:
+                words = self.stopwords(words, stoplist)
+            #  Add the words list to a larger list.
+            self.totaldata.append(words)
+
+        print("*  all data processed.")
+
+        print("*  making vocab.")
+        self.vocab()
+        print("* done with vocab.")
 
     def tokenize(self, file):
         #  Remove the html tags.
@@ -69,12 +80,35 @@ class BagofWords(object):
         for word in words:
             if x == self.ngrams - 1:
                 nwords = nwords + word
-                x = x + 1
+                x += 1
             elif x < self.ngrams:
                 nwords =  nwords + word + ' '
-                x = x + 1
+                x += 1
             else:
                 wordsnew.append(nwords)
                 nwords = word + ' '
                 x = 1
         return wordsnew
+
+    def vocab(self):
+        for data in self.totaldata:
+            for word in data:
+                self.wordcount += 1
+                if word in self.vocabulary:
+                    self.vocabulary[word] += 1
+                else:
+                    self.vocabulary[word] = 1
+
+    def reset(self, fp, stopfp, ngrams):
+        self.fp = fp
+        self.stopfp = stopfp
+        self.ngrams = ngrams
+        self.totaldata = []
+        self.vocabulary = {}
+        self.wordcount = 0
+
+    def get_vocabulary(self):
+        return self.vocabulary
+
+    def get_wordcount(self):
+        return self.vocabulary
